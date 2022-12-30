38263fc84c0767784413017d76a275d1e4556c63
erenbekmezci <eren_bekmezci@hotmail.com.tr>
2022-12-30 21:37:34 +0300

    Sepet islemleri

---
+++ teknofest/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess.Abstract;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace teknofest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        private IFoodDal service;
+        public ValuesController(IFoodDal _service)
+        {
+            service = _service;
+        }
+        [HttpGet]
+        public IEnumerable<Food> Get()
+        {
+            return service.GetAll();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
