30753e7b7af96bfbce3f03ac4d985a0043fb8d88
erenbekmezci <eren_bekmezci@hotmail.com.tr>
2023-01-01 23:59:13 +0300

    api son

---
+++ teknofest/Controllers/CallDesertApi.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace teknofest.Controllers
+{
+    public class CallDesertApi : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<FoodCategory> foodCategories = new List<FoodCategory>();
+            var hhtc = new HttpClient();
+            var response = await hhtc.GetAsync("https://localhost:7220/api/DesertApi");
+            string resString = await response.Content.ReadAsStringAsync();
+            foodCategories = JsonConvert.DeserializeObject<List<FoodCategory>>(resString);
+            return View(foodCategories);
+        }
+    }
+}
