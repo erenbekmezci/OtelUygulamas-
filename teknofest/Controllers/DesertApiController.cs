467e5045f034b09a4922bd99ee734487042c35c1
erenbekmezci <eren_bekmezci@hotmail.com.tr>
2023-01-01 23:46:01 +0300

    api

---
+++ teknofest/Controllers/DesertApiController.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Concrete.EfCore;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace teknofest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        OtelContext db = new OtelContext();
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<ActionResult<List<FoodCategory>>> Get()
+        {
+            var y = await db.FoodCategories.Where(i=>i.Name.Contains("Tatlı")).Include(i=> i.Foods).ToListAsync();
+            if (y is null)
+            {
+                return NoContent();
+            }
+            return y;
+
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
